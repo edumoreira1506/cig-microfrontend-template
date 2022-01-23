b2026ef8e425a72d51a22edecdeccce44967e60c
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-23 17:17:21 -0300

    Setup eslint

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-import { Header } from './Header';
-import './page.css';
+import { Header } from './Header'
+import './page.css'
 
 interface PageProps {
-  user?: {};
+  user?: any;
   onLogin: () => void;
   onLogout: () => void;
   onCreateAccount: () => void;
@@ -30,8 +30,7 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }: PageProps) =>
       </p>
       <ul>
         <li>
-          Use a higher-level connected component. Storybook helps you compose such data from the
-          "args" of child component stories
+          Use a higher-level connected component. Storybook helps you compose such data from the args of child component stories
         </li>
         <li>
           Assemble data in the page component from your services. You can mock these services out
@@ -64,4 +63,4 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }: PageProps) =>
       </div>
     </section>
   </article>
-);
+)
