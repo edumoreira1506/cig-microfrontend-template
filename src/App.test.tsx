937de56a49978b0f9a2f522061183d803ab2f19a
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-09 11:12:15 -0300

    Bump dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,11 @@
-it('example', () => {
-  expect(true).toBeTruthy()
+import { render, screen } from "@testing-library/react"
+
+import App from "./App"
+
+describe('<App />', () => {
+  it('renders correctly' , () => {
+    render(<App />)
+
+    expect(screen.getByText('App')).toBeInTheDocument
+  })
 })
